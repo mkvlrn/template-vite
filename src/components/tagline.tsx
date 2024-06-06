dcb552ffe497cd9afb56e34355a1862387d73275
Mike Valeriano <mkvlrn@gmail.com>
2024-06-06 09:39:23 -0300

    chore: bump deps, adjust lint-staged

--- src/components/tagline.tsx
+++ src/components/tagline.tsx
@@ -1,6 +1,6 @@
 export function Tagline() {
   return (
-    <h3 className='text-center text-xl mb-4'>
+    <h3 className='mb-4 text-center text-xl'>
       An opinionated boilerplate for SPA React projects built with Vite.
     </h3>
   );
