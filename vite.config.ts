f9449fd29e8424ec5a83c223b1e8e2c9fe30e07c
Mike Valeriano <mkvlrn@gmail.com>
2024-09-04 15:46:27 -0300

    chore: bump deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import * as react from "@vitejs/plugin-react";
 import { defineConfig, mergeConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig as defineTestConfig } from "vitest/config";
+import { compilerOptions } from "./tsconfig.json";
+
+const { outDir } = compilerOptions;
 
 const baseConfig = defineConfig({
   plugins: [react.default(), tsconfigPaths()],
   server: { port: 3000 },
+  build: {
+    outDir,
+  },
 });
 
 const testConfig = defineTestConfig({
